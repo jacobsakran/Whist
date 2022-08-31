3b13bf7b23babad03173e778cbbaf0fe1a9067ee
jacobsakran <jacob.sakran@gmail.com>
2022-08-31 11:49:34 +0300

    updated

--- app/src/main/java/com/example/whist20/top_players.java
+++ app/src/main/java/com/example/whist20/top_players.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +16,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class top_players extends AppCompatActivity {
 
@@ -23,7 +29,9 @@ public class top_players extends AppCompatActivity {
     private User first = null;
     private User sec = null;
     private User third = null;
+    private TextView first_text, sec_text, third_text;
     private Node friends;
+    private Lock mutex;
 
 
     @Override
@@ -32,7 +40,10 @@ public class top_players extends AppCompatActivity {
         setContentView(R.layout.activity_top_players);
 
         back = (Button) findViewById(R.id.back_top_players);
-
+        first_text = (TextView) findViewById(R.id.firstPlayer);
+        sec_text = (TextView) findViewById(R.id.secondPlayer);
+        third_text = (TextView) findViewById(R.id.thirdPlayer);
+        mutex = new ReentrantLock();
         back.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -47,11 +58,48 @@ public class top_players extends AppCompatActivity {
             FirebaseDatabase.getInstance().getReference("Users").child((String) iterator.obj).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    User friend = snapshot.getValue(User.class);
-                    assert friend != null;
-                    if (first != null) {
-                        
+                    mutex.lock();
+                    try {
+                        User friend = snapshot.getValue(User.class);
+                        assert friend != null;
+                        if (first == null) {
+                            first = new User();
+                            first.username = friend.username;
+                            first.uid = friend.uid;
+                            first.rank = friend.rank;
+                        }
+                        if (sec == null) {
+                            sec = new User();
+                            sec.username = friend.username;
+                            sec.uid = friend.uid;
+                            sec.rank = friend.rank;
+                        }
+                        if (third == null) {
+                            third = new User();
+                            third.username = friend.username;
+                            third.uid = friend.uid;
+                            third.rank = friend.rank;
+                        }
+                        if (friend.rank > first.rank) {
+                            first.username = friend.username;
+                            first.uid = friend.uid;
+                            first.rank = friend.rank;
+                        } else if (friend.rank > sec.rank) {
+                            sec.username = friend.username;
+                            sec.uid = friend.uid;
+                            sec.rank = friend.rank;
+                        } else if (friend.rank > third.rank) {
+                            third.username = friend.username;
+                            third.uid = friend.uid;
+                            third.rank = friend.rank;
+                        }
+                        first_text.setText(first.username);
+                        sec_text.setText(sec.username);
+                        third_text.setText(third.username);
+                    } finally {
+                        mutex.unlock();
                     }
+
                 }
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
@@ -59,18 +107,6 @@ public class top_players extends AppCompatActivity {
                 }
             });
             iterator = iterator.next;
-        }
-
-        if (Objects.equals(user.uid, first.uid)) {
-            Toast.makeText(top_players.this, "congrats you got the first place!", Toast.LENGTH_LONG).show();
-
-        }
-        if (Objects.equals(user.uid, sec.uid)) {
-            Toast.makeText(top_players.this, "congrats you got the second place!", Toast.LENGTH_LONG).show();
-
-        }
-        if (Objects.equals(user.uid, third.uid)) {
-            Toast.makeText(top_players.this, "congrats you got the third place!", Toast.LENGTH_LONG).show();
         }
 
     }
